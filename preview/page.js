5e50a561515071875fada1b13bd9d98d4c5dc14a
Jesse Grant <jesse.grant@gmail.com>
2013-01-23 12:00:13 -0500

    Initial commit for Notes Plugin Pages

---
+++ preview/page.js
@@ -0,0 +1,6 @@
+$(function(){
+  var content = JSON.parse(BL.getContentForPreview().content[0].data);
+  $('#text').append('' + content.body + '');
+  $('#author').append('-' + content.author + '');
+  BL.previewReady();
+});
