77773bc279703e3643593f1d5e98cf5454956727
Jesse Grant <jesse.grant@gmail.com>
2013-01-23 12:19:27 -0500

    Formatting

--- create/page.js
+++ create/page.js
@@ -1,6 +1,6 @@
-$(function(){
-  $('#save-button').click(function(e){
-    var content = {body: $('#note-body').val(), author: $('#note-author').val()};
+$(function () {
+  $('#save-button').click(function (e) {
+    var content = {body:$('#note-body').val(), author:$('#note-author').val()};
     BL.createContent(JSON.stringify(content));
   })
 });
--- preview/page.js
+++ preview/page.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
   var content = JSON.parse(BL.getContentForPreview().content[0].data);
   $('#text').append('' + content.body + '');
   $('#author').append('-' + content.author + '');
